d58a5a7f9898513e687a8d2ab4adf89880130e20
Jabriel Hans Talula <145314061+jirbthagoras@users.noreply.github.com>
2024-11-23 08:23:53 +0700

    Update Configuration.jsx

--- src/Configuration.jsx
+++ src/Configuration.jsx
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCXzcpUubhO0IsF9EkbYcv5yUCYTQilWfI",
+    apiKey: "",
     authDomain: "react-firebase-41f3e.firebaseapp.com",
     databaseURL: "https://react-firebase-41f3e-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "react-firebase-41f3e",
